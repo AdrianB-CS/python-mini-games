5306ac716e05c8adf4083d7418d74d52dde7fad8
Adrian Benjamin <adrianbenjamincs@gmail.com>
2026-01-04 15:18:49 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,57 +1,27 @@
-import random
-import hangman_words
-import hangman_art
+from art import logo
+print(logo)
 
-words = random.choice(hangman_words.word_list)
-lives = 6
 
-print(hangman_art.logo)
-chosen_word = random.choice(hangman_words.word_list)
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
 
-placeholder = ""
-word_length = len(chosen_word)
-for position in range(word_length):
-    placeholder += "_"
-print("Word to guess: " + placeholder)
 
-game_over = False
-correct_letters = []
-wrong_letters = []
-
-while not game_over:
-
-    print("****************************" + str(lives) + " LIVES LEFT****************************")
-    guess = input("Guess a letter: ").lower()
-
-    if guess in correct_letters:
-        print("You have already guessed this letter, please try again.")
-    display = ""
-
-    for letter in chosen_word:
-        if letter == guess:
-            display += letter
-            correct_letters.append(guess)
-        elif letter in correct_letters:
-            display += letter
-        else:
-            display += "_"
-
-    print("Word to guess: " + display)
-
-    if guess not in chosen_word:
-        lives -= 1
-        wrong_letters.append(guess)
-        print("You guessed " + str(guess) + ", that's not in the word. You lose a life.")
-        print("You have " + str(lives) + " lives left.")
-        print("You have guessed " + str(wrong_letters) + " so far.")
-        if lives == 0:
-            game_over = True
-
-            print(f"***********************YOU LOSE**********************")
-            print("You were trying to guess " + str(chosen_word))
-
-    if "_" not in display:
-        game_over = True
-        print("****************************YOU WIN****************************")
-
-    print(hangman_art.stages[lives])
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 40)
